4716d1d5822110ccc3b2e6f16249dd31160bdbe6
Jon Sousa <jonathonsousa@gmail.com>
2023-09-24 10:03:28 -0500

    Added prisma

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -6,7 +6,6 @@ const ProductCard = () => {
     
   return (
     <>
-      
       <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import ProductCard from './components/ProductCard/ProductCard'
 
 //Start here: https://members.codewithmosh.com/courses/mastering-next-js-13-with-typescript/lectures/49120345
+//TODO: Add this: https://www.youtube.com/watch?v=DCRcFf39SYo
 export default function Home() {
   return (
     <main>
