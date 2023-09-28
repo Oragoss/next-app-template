2b7f0ab41d9606b8010470a5de2be81786ef59f0
Jon Sousa <jonathonsousa@gmail.com>
2023-09-28 18:03:07 -0500

    Can use credentials to sign in

--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -3,6 +3,7 @@
 import React, { ReactNode } from 'react'
 import { SessionProvider } from 'next-auth/react'
 
+//This is the session provider wrapper. Maybe make this more explicit?
 const AuthProvider = ({ children }: { children:ReactNode }) => {
   return (
     <SessionProvider>
