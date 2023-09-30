7a33f80ac0d22cf92ae0705c0c9bf55a31ef7582
Jon Sousa <jonathonsousa@gmail.com>
2023-09-30 13:11:46 -0500

    Added authOptions as its own file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt';
-
-const prisma = new PrismaClient();
-
-// //The ! tells the typescript compiler that we def have assigned a variable here and chill out
-//Docs: https://next-auth.js.org/configuration/initialization#route-handlers-app
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: { label: 'Email', type: 'email', placeholder: 'Enter your email here'},
-                password: { label: 'Password', type: 'password', placeholder: 'Enter your password here'}
-            },
-            async authorize(credentials, req) {
-                if (!credentials?.email || !credentials?.password) return null;
-
-                const user = await prisma.user.findUnique({
-                    where: { email: credentials.email }
-                });
-
-                if(!user) return null;
-
-               const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!);
-
-               return passwordsMatch ? user : null;
-            }
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    session: {
-        strategy: 'jwt'
-    }
-};
+import NextAuth from "next-auth";
+import { authOptions } from "../authOptions";
 
 const handler = NextAuth(authOptions);
 
--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'daisyui.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
   //   /**
   //    https://github.com/gregrickaby/nextjs-github-pages
   //  * Enable static exports for the App Router.
